218020ea8f5989aef75a26640c62b8bdc7bfe50e
rtm516 <ryantmilner@hotmail.co.uk>
2022-02-06 21:55:35 +0000

    Add update timer

--- CoreTool/Program.cs
+++ CoreTool/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace CoreTool
 {
     class Program
     {
+        private static Timer updateTimer;
+        private static ArchiveMeta archiveMeta;
 
         static async Task Main(string[] args)
         {
@@ -19,7 +22,7 @@ namespace CoreTool
             // The user doesn't need to know this (useful for debugging)
             //Console.WriteLine($"Got token: {token}");
 
-            ArchiveMeta archiveMeta = new ArchiveMeta(archiveDir);
+            archiveMeta = new ArchiveMeta(archiveDir);
 
             // Pull updates
             archiveMeta.LoadFiles();
@@ -30,7 +33,30 @@ namespace CoreTool
             archiveMeta.CheckMeta();
             await archiveMeta.CheckFiles(token);
 
+            Console.WriteLine("Done startup!");
+            Console.WriteLine("Starting update checker");
+
+            // Check for updates every 5 mins
+            updateTimer = new Timer(5 * 60 * 1000);
+            updateTimer.Elapsed += OnUpdateEvent;
+            updateTimer.AutoReset = true;
+            updateTimer.Enabled = true;
+
             Console.ReadLine();
+        }
+
+        private static async void OnUpdateEvent(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Checking for updates...");
+
+            // Grab a new token incase the other expired
+            string token = Authentication.GetWUToken();
+
+            // Load the live data
+            await archiveMeta.LoadLive(token);
+
+            // Pull any missing files
+            await archiveMeta.CheckFiles(token);
         }
     }
 }
