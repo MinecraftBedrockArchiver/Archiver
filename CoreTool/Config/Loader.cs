e3594bb4052cfeeed27c7db1268fff6e3303dd78
rtm516 <ryantmilner@hotmail.co.uk>
2022-02-10 00:50:21 +0000

    Allow manual build triggers and add config load message

--- CoreTool/Config/Loader.cs
+++ CoreTool/Config/Loader.cs
@@ -12,6 +12,8 @@ namespace CoreTool.Config
 
         public static List<ArchiveMeta> Load()
         {
+            Utils.GenericLogger.Write("Loading config from " + configFile);
+
             if (!File.Exists(configFile))
             {
                 File.WriteAllText(configFile, "[\n  {\n    \"Name\": \"W10\",\n    \"Directory\": \".\\\\Windows10 - Microsoft.MinecraftUWP_8wekyb3d8bbwe\\\\\",\n    \"Loaders\": [\n      {\n      \"Class\": \"CoreTool.Loaders.FileLoader\"\n      },\n      {\n      \"Class\": \"CoreTool.Loaders.VersionDBLoader\"\n      },\n      {\n      \"Class\": \"CoreTool.Loaders.StoreLoader\",\n      \"Params\": [\n        \"9NBLGGH2JHXJ\",\n        \"Microsoft.MinecraftUWP\"\n      ]\n      }\n    ],\n    \"Checkers\": [\n      {\n      \"Class\": \"CoreTool.Checkers.MetaChecker\"\n      },\n      {\n      \"Class\": \"CoreTool.Checkers.FileChecker\"\n      }\n    ]\n  }\n]");
