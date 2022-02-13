fb33732520447f015c1c1efec20004ac0df7eefc
rtm516 <ryantmilner@hotmail.co.uk>
2022-02-13 00:27:36 +0000

    Add DataStoreExtractor

---
+++ DataStoreExtractor/Authentication.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace DataStoreExtractor
+{
+    // TODO: Move this to a shared lib? Same as the one in CoreTool
+    class Authentication
+    {
+        [DllImport("WUTokenHelper.dll", CallingConvention = CallingConvention.StdCall)]
+        private static extern int GetWUToken(string scope, [MarshalAs(UnmanagedType.LPWStr)] out string token);
+
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Auto, SetLastError = true)]
+        static public extern IntPtr GetStdHandle(uint nStdHandle);
+
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Auto, SetLastError = true)]
+        static public extern bool SetStdHandle(uint nStdHandle, IntPtr hHandle);
+
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Auto, SetLastError = true)]
+        static public extern IntPtr CreateConsoleScreenBuffer(uint dwDesiredAcess, uint dwShareMode, IntPtr lpSecurityAttributes, uint dwFlags, IntPtr lpScreenBufferData);
+
+        const uint accessMode = 0x40000000; // GENERIC_WRITE
+        const uint STD_OUTPUT_HANDLE = unchecked((uint)-11);
+        const uint STD_ERROR_HANDLE = unchecked((uint)-12);
+
+        /// <summary>
+        /// Fetch the WU token from either a token.txt or using the WUTokenHelper.dll
+        /// </summary>
+        /// <returns>A WU token</returns>
+        /// <exception cref="Win32Exception"></exception>
+        public static string GetWUToken(string scope = "service::dcat.update.microsoft.com::MBI_SSL")
+        {
+            String token;
+
+            // Redirect console output to nothing so we hide the dll output
+            // From: https://social.msdn.microsoft.com/Forums/en-US/c89e2f26-9fb2-46f3-a138-591cb91c5105/#5a6fc798-4ed4-464d-b6bd-81c22fb9771f-isAnswer
+            // TODO: Find out why this breaks in release builds of the dll
+            IntPtr hBuffer = CreateConsoleScreenBuffer(accessMode, 0, IntPtr.Zero, 1, IntPtr.Zero);
+            IntPtr hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+            IntPtr hStdErr = GetStdHandle(STD_ERROR_HANDLE);
+            if (SetStdHandle(STD_OUTPUT_HANDLE, hBuffer) && SetStdHandle(STD_ERROR_HANDLE, hBuffer))
+            {
+                // Get the token using the token helper
+                if (GetWUToken(scope, out token) != 0)
+                {
+                    token = "";
+                }
+
+                // Restore original console buffers
+                if (!SetStdHandle(STD_OUTPUT_HANDLE, hStdOut) || !SetStdHandle(STD_ERROR_HANDLE, hStdErr))
+                {
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+                }
+            }
+            else
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+
+            return token;
+        }
+    }
+}
---
+++ DataStoreExtractor/Program.cs
@@ -0,0 +1,219 @@
+﻿using Microsoft.Isam.Esent.Interop;
+using StoreLib.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DataStoreExtractor
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                // Copy the datastore to our current dir
+                File.Copy(@"C:\Windows\SoftwareDistribution\DataStore\DataStore.edb", "DataStore.edb", true);
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Unable to copy the DataStore.edb, make sure the Windows Update (wuauserv) service");
+                Environment.Exit(1);
+            }
+
+            string[] wantedIds = new string[] {
+                "9NBLGGH2JHXJ", // Microsoft.MinecraftUWP
+	            "9P5X4QVLC2XR" // Microsoft.MinecraftWindowsBeta
+            };
+
+            Dictionary<Guid, string> updatePackageMap = new Dictionary<Guid, string>();
+
+            // Setup the edb reading
+            #region Load DB
+            JET_INSTANCE instance;
+            JET_SESID sesid;
+            JET_DBID dbid;
+            JET_TABLEID tableid;
+            IDictionary<string, JET_COLUMNID> colMap;
+
+            SystemParameters.DatabasePageSize = 16 * 1024;
+
+            Api.JetCreateInstance(out instance, Guid.NewGuid().ToString());
+            Api.JetInit(ref instance);
+
+            Api.JetBeginSession(instance, out sesid, null, null);
+
+            Api.JetAttachDatabase(sesid, @"DataStore.edb", AttachDatabaseGrbit.ReadOnly);
+
+            Api.OpenDatabase(sesid, @"DataStore.edb", out dbid, OpenDatabaseGrbit.ReadOnly);
+            #endregion
+
+            // Pull from history
+            Api.OpenTable(sesid, dbid, "tbHistory", OpenTableGrbit.None, out tableid);
+
+            colMap = Api.GetColumnDictionary(sesid, tableid);
+
+            do
+            {
+                string title = Api.RetrieveColumnAsString(sesid, tableid, colMap["Title"]);
+                Guid updateId = new Guid(Api.RetrieveColumn(sesid, tableid, colMap["UpdateId"]).Take(16).ToArray());
+
+                if (wantedIds.Contains(title.Split('-')[0]))
+                {
+                    updatePackageMap[updateId] = title;
+                }
+            }
+            while (Api.TryMoveNext(sesid, tableid));
+
+            // Pull from updates
+            Api.OpenTable(sesid, dbid, "tbUpdates", OpenTableGrbit.None, out tableid);
+
+            colMap = Api.GetColumnDictionary(sesid, tableid);
+
+            Dictionary<int, Guid> updateMap = new Dictionary<int, Guid>();
+
+            do
+            {
+                int idLocal = Api.RetrieveColumnAsInt32(sesid, tableid, colMap["IdLocal"]).Value;
+                Guid updateId = new Guid(Api.RetrieveColumn(sesid, tableid, colMap["UpdateId"]).Take(16).ToArray());
+
+                updateMap[idLocal] = updateId;
+            }
+            while (Api.TryMoveNext(sesid, tableid));
+
+            // Collate the names with the pulled updates
+            Api.OpenTable(sesid, dbid, "tbUpdateLocalizedProps", OpenTableGrbit.None, out tableid);
+
+            colMap = Api.GetColumnDictionary(sesid, tableid);
+
+            do
+            {
+                string title = Api.RetrieveColumnAsString(sesid, tableid, colMap["Title"]);
+                int idLocal = Api.RetrieveColumnAsInt32(sesid, tableid, colMap["IdLocal"]).Value;
+
+                if (wantedIds.Contains(title.Split('-')[0]))
+                {
+                    updatePackageMap[updateMap[idLocal]] = title;
+                }
+            }
+            while (Api.TryMoveNext(sesid, tableid));
+
+            // Tell the user the IDs that we have found
+            foreach (var item in updatePackageMap)
+            {
+                Console.WriteLine($"{item.Value} -> {item.Key}");
+            }
+
+            // Ask the user what they want to do
+            Console.WriteLine();
+            Console.Write("Do you want to try to download these and check they are valid? ");
+            if (Console.ReadKey().KeyChar != 'y')
+            {
+                Console.WriteLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Do you want to delete valid files after download? ");
+            bool cleanUp = false;
+            if (Console.ReadKey().KeyChar == 'y')
+            {
+                cleanUp = true;
+            }
+
+            Console.WriteLine();
+
+            // Get the URLs
+            List<string> updateIds = updatePackageMap.Keys.Select(x => x.ToString()).ToList();
+            List<string> revisionIds = Enumerable.Repeat("1", updateIds.Count).ToList();
+
+            IList<Uri> Files = await FE3Handler.GetFileUrlsAsync(updateIds, revisionIds, $"<User>{Authentication.GetWUToken()}</User>"); // TODO: Maybe add auth here?
+
+            WebClient wc = new WebClient();
+            wc.DownloadProgressChanged += (s, e) => Console.Write("\r{0}%", e.ProgressPercentage);
+
+            Dictionary<string, string> validUpdates = new Dictionary<string, string>();
+            string downloadFolder = "./Downloads/";
+            Directory.CreateDirectory(downloadFolder);
+
+            // Download and check each update
+            int i = 0;
+            foreach (Uri uri in Files)
+            {
+                if (uri.Host == "test.com")
+                {
+                    i++;
+                    continue;
+                }
+
+                string downloadLocation = Path.Join(downloadFolder, updateIds[i]);
+
+                Console.WriteLine($"Downloading {updateIds[i]}");
+                await wc.DownloadFileTaskAsync(uri, downloadLocation);
+
+                Console.WriteLine();
+
+                Console.WriteLine($"Checking");
+                bool valid = false;
+                string outName = "";
+                try
+                {
+                    // Read the downloaded file as a zip
+                    // This will only work with appx files so we can validate the download
+                    using (ZipArchive zip = ZipFile.Open(downloadLocation, ZipArchiveMode.Read))
+                    {
+                        foreach (ZipArchiveEntry entry in zip.Entries)
+                        {
+                            if (entry.Name == "AppxManifest.xml")
+                            {
+                                // Load the AppxManifest to work out the filename
+                                XmlDocument doc = new XmlDocument();
+                                doc.Load(entry.Open());
+
+                                XmlNode identity = doc.GetElementsByTagName("Identity")[0];
+                                outName = $"{identity.Attributes["Name"].Value}_{identity.Attributes["Version"].Value}_{identity.Attributes["ProcessorArchitecture"].Value}__8wekyb3d8bbwe.Appx";
+
+                                validUpdates.Add(updateIds[i], outName);
+                                valid = true;
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("Valid");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Invalid");
+                    Console.WriteLine(ex.Message);
+                    File.Delete(downloadLocation);
+                }
+
+                // Cleanup the file if needed or rename
+                if (cleanUp && valid)
+                {
+                    File.Delete(downloadLocation);
+                }
+                else if (valid)
+                {
+                    File.Move(downloadLocation, Path.Join(downloadFolder, outName), true);
+                }
+
+                i++;
+            }
+
+            // Tell the user the final packages
+            Console.WriteLine();
+            Console.WriteLine("Finished downloads:");
+            foreach (string update in validUpdates.Keys)
+            {
+                Console.WriteLine($"{update} -> {validUpdates[update]}");
+            }
+        }
+    }
+}
